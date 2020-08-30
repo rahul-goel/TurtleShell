a53f5331b28a43c90b4b69ccfc3625346123199b
rahul-goel <rahulgoellko@gmail.com>
2020-08-30 22:42:30 +0530

    cd working

--- cd.c
+++ cd.c
@@ -1,28 +1,58 @@
 #include "header.h"
 
 void cd(char *path) {
-    if (path[0] == '~') {
-        chdir(HOME);
-        if (strlen(path) > 2) {
-            path = path + 2;
-        }
+    char *token = (char *) malloc(sizeof(path) + 5);
+    strcpy(token, path);
+    token = strtok(token, " \t\n\r");
+    
+    // if the absolute path through root is given
+    token = strtok(NULL, " \t\n\r");
+    if (token[0] == '/') {
+        chdir("/");
     }
+    getcwd(PWD, sizeof PWD);
 
-    if (chdir(path) == -1) {
-        perror("Error: Specified Directory does not exist.\n");
-        return;
+    // reset the token
+    strcpy(token, path);
+    token = strtok(token, " \t\n\r");
+    token = strtok(NULL, "/\n");
+
+
+    // store the PWD to return incase of error
+    char store_PWD[2048];
+    strcpy(store_PWD, PWD);
+
+    while (token != NULL) {
+        if (strcmp(token, "~") == 0) {
+            chdir(HOME);
+        } else if (chdir(token) == -1) {
+            chdir(store_PWD);
+            perror("Error");
+            free(token);
+            return;
+        }
+        token = strtok(NULL, "/\n");
     }
 
     getcwd(PWD, sizeof PWD);
     printf("%s\n", PWD);
+    free(token);
+    return;
 }
 
 /*
+
 int main() {
+    getcwd(PWD, sizeof PWD);
     getcwd(HOME, sizeof HOME);
-    char str[] = "~/juicy/fruit pudding";
-    cd(str);
+    while (1) {
+        char *command = (char *) malloc(1024 * sizeof(char));
+        size_t n = 1024;
+        getline(&command, &n, stdin);
+        cd(command);
+    }
     return 0;
 }
+
 */
 

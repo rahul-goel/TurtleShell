3c9e70f9e8e7d59ead6056c165b033bef2cf475e
rahul-goel <rahulgoellko@gmail.com>
2020-08-30 19:15:41 +0530

    echo finished

--- echo.c
+++ echo.c
@@ -1,6 +1,7 @@
 #include "header.h"
 
 int print_env_var(char *v) {
+    v++;
     if (strcmp(v, HOST_str) == 0) {
         printf("%s ", HOST);
     } else if (strcmp(v, USER_str) == 0) {
@@ -18,14 +19,36 @@ int print_env_var(char *v) {
     return 0;
 }
 
-void echo(char *str[], int cnt) {
-    for (int i = 0; i < cnt; i++) {
-        if (*str[i] == '$') {
-            if (print_env_var(str[i] + 1) == -1) {
-                return;
-            }
-        } else {
-            printf("%s ", str[i]);
-        }
+// takes in one argument from the command
+// handles environment variables as well
+void echo_single(char* str) {
+    if (strcmp(str, "$") == 0) {
+        printf("$");
+    } else if(str[0] == '$') {
+        print_env_var(str);
+    } else {
+        printf("%s ", str);
     }
 }
+
+// takes in the command which includes the echo keyword
+void echo(char *line) {
+    char *token = line;
+    token = strtok(token, " \n\t\r");
+    token = strtok(NULL, " \n\t\r");
+    while (token != NULL) {
+        echo_single(token);
+        token = strtok(NULL, " \n\t\r");
+    }
+    printf("\n");
+}
+
+/*
+int main(int argv, char *argc[]) {
+    char *str = (char *) malloc(1024 * sizeof(char));
+    size_t n = 1024;;
+    getline(&str, &n, stdin);
+    echo(str);
+    return 0;
+}
+*/
